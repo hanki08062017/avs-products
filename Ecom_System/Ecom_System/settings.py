bb6df28e36f90fcaf58660973c1bf3c9da31b3f2
Harendra Sharma <hanki08062017@gmail.com>
2026-03-23 23:08:53 +0530

    publishing settings

--- Ecom_System/Ecom_System/settings.py
+++ Ecom_System/Ecom_System/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -75,5 +76,7 @@ USE_I18N      = True
 USE_TZ        = True
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
